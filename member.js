32436e10c6cba772ab5e3fdbe7a42f806d795947
lalors <38389634+lalors@users.noreply.github.com>
2025-01-22 02:07:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        job: 'Software Engineer',
+        age: 30,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB', 'Express']
+    }
+    return member;
+}
